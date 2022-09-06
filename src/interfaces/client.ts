1499f854f40f490f9448920af95c9f76169c62e8
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-06 19:07:32 +0200

    Added client interface.

---
+++ src/interfaces/client.ts
@@ -0,0 +1,11 @@
+export interface Client {
+  id: string;
+  userId: string;
+  companyName: string;
+  streetAddress: string;
+  cityAndCode: string;
+  nip: number;
+  regon: number;
+  email: string;
+  phoneNumber: number;
+}
