eae3d60262d4b27b8e932db2c0dfe859a5113d1d
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-11 10:25:32 +0200

    Added items dto.

---
+++ src/items/items.dto.ts
@@ -0,0 +1,37 @@
+import { IsNumber, IsString, Max } from 'class-validator';
+
+export class ItemsDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  invoiceId: string;
+
+  @IsString()
+  @Max(255)
+  name: string;
+
+  @IsNumber()
+  @Max(6)
+  quantity: number;
+
+  @IsNumber()
+  @Max(12)
+  netValue: number;
+
+  @IsNumber()
+  @Max(14)
+  totalNetValue: number;
+
+  @IsNumber()
+  @Max(6)
+  taxRate: number;
+
+  @IsNumber()
+  @Max(12)
+  taxValue: number;
+
+  @IsNumber()
+  @Max(14)
+  totalGrossValue: number;
+}
