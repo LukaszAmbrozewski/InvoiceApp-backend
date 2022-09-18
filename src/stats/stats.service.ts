a39dd8976872df15fe3e270f8b0483f8c51c3dc0
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-18 12:59:42 +0200

    Added get stats function.

--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { MonthStats, Period } from 'src/interfaces/stats';
+import { User } from '../user/user.entity';
+import { stats } from '../utils/get-stats';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  async getStats(user: User, period: Period): Promise<MonthStats[]> {
+    console.log(user);
+    return stats(user, period.startingDate, period.endingDate);
+  }
+}
