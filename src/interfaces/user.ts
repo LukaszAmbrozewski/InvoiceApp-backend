484c8f50aa2644ddbaa31a2ff2f997ce5d7b4bcd
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-18 21:12:11 +0200

    Added user response type.

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -2,3 +2,14 @@ export interface RegisterUserResponse {
   email: string;
   id: string;
 }
+
+export type UserDataResponse = {
+  id: string;
+  email: string;
+  companyName: string;
+  streetAddress: string;
+  cityAndCode: string;
+  nip: number;
+  regon: number;
+  phoneNumber: number;
+};
