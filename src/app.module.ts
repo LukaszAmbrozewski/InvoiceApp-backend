577635acd94c5506916fac1068d747dccc9add95
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-09-18 13:31:28 +0200

    Generated history module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,10 +8,12 @@ import { ClientsModule } from './clients/clients.module';
 import { InvoicesModule } from './invoices/invoices.module';
 import { ItemsModule } from './items/items.module';
 import { StatsModule } from './stats/stats.module';
+import { HistoryController } from './history/history.controller';
+import { HistoryModule } from './history/history.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule, ClientsModule, InvoicesModule, ItemsModule, StatsModule],
-  controllers: [AppController],
+  imports: [DatabaseModule, UserModule, AuthModule, ClientsModule, InvoicesModule, ItemsModule, StatsModule, HistoryModule],
+  controllers: [AppController, HistoryController],
   providers: [AppService],
 })
 export class AppModule {}
