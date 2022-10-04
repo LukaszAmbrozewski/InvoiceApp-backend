7a7ab1058c2a90ba42fd10816bd2c1de388b34a2
LukaszAmbrozewski <lukaszambrozewski@wp.pl>
2022-10-04 14:14:25 +0200

    Added cors and updated packages.

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   (app as NestExpressApplication).use(helmet());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
